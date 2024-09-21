ab63fda798c2be655050103b4c16ee615b061594
yujin <northwindapps@gmail.com>
2024-09-21 22:34:21 +0900

    added template

--- resources/src/index.jsx
+++ resources/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import ReactDOM from "react-dom/client";
-import ReactDOM from "react-dom"; 
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,13 +13,29 @@ import AdminLayout from './layouts/Admin.jsx';
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  ReactDOM.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminLayout />} />
-        <Route path="/" element={<Navigate to="/admin/dashboard" />} />
-      </Routes>
-    </BrowserRouter>,
-    document.getElementById('root') // Specify the root element here
-  );
+// if (document.getElementById('root')) {
+//   root.render(
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/admin" element={<AdminLayout />} />
+//         <Route path="/" element={<Navigate to="/admin/dashboard" />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
+if (document.getElementById('root')) {
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container); // Create a root
+  root.render( <div>
+      <h1>Hello, React in Laravel!?</h1>
+  </div>); // Render your component with createRoot
+}
+
+// root.render(<BrowserRouter>
+//   <Routes>
+//     <Route path="/admin" element={<AdminLayout />} />
+//     <Route path="/" element={<Navigate to="/admin/dashboard" />} />
+//   </Routes>
+// </BrowserRouter>); // Render your component with createRoot
+// }
