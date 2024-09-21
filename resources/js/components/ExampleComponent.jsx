12270586508da2347c366d80040c611bdfaa7fe2
yujin <northwindapps@gmail.com>
2024-09-21 16:12:41 +0900

    changed to reaxt components

---
+++ resources/js/components/ExampleComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExampleComponent() {
+    return (
+        <div>
+            <h1>Hello, React in Laravel!</h1>
+        </div>
+    );
+}
+
+export default ExampleComponent;
