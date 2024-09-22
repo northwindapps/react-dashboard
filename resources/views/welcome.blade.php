865ea37a9cb944114897a035b848054a91401341
yujin <northwindapps@gmail.com>
2024-09-22 18:56:41 +0900

    introduced VITE_ENABLED in blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Your App</title>
-    <!-- Vite directives for React refresh and app assets -->
-    @viteReactRefresh
-    @vite('resources/src/index.jsx') <!-- If your main JS file is renamed to app.jsx -->
+<title>Your App</title>
+    @if (env('VITE_ENABLED', true))
+        @viteReactRefresh
+        @vite('resources/src/index.jsx') <!-- If your main JS file is renamed to app.jsx -->
+    @endif
 </head>
 <body>
     <div id="root"></div>
