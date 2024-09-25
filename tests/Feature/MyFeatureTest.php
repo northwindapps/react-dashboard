f4ae3ebf308645071a56e35790bb3a219e55c664
yujin <northwindapps@gmail.com>
2024-09-25 20:15:01 +0900

    added some unittests

--- tests/Feature/MyFeatureTest.php
+++ tests/Feature/MyFeatureTest.php
@@ -5,9 +5,13 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class MyFeatureTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
@@ -16,5 +20,13 @@ class MyFeatureTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_example2(): void
+    {
+        $this->seed(); 
+        $emailToCheck = 'test@example.com';
+        $userCount = DB::table('users')->where('email', $emailToCheck)->count();
+        $this->assertEquals(1, $userCount);
     }
 }
