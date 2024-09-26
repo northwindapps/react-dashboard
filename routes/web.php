b573f1d28c29634ec9ce206c6d41a61e9f0b219c
yujin <northwindapps@gmail.com>
2024-09-26 19:21:29 +0900

    deleted trash

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/admin/dashboard', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/user', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/maps', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/table', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/typography', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/icons', function () {
-    return view('welcome');
-});
-
-Route::get('/admin/notifications', function () {
-    return view('welcome');
-});
